8875119d2c53015b51d1f5489f8bde4ca1812784
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-12-15 01:33:43 +0530

    add tailwind css and add a Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Header from './component/Header/Header';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Signup from './Pages/Signup';
+import Dashboard from './Pages/Dashboard';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Signup></Signup>}></Route>
+          <Route path='/dashboard' element = {<Dashboard></Dashboard>}></Route>
+         
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'custom-pink': '#FF748B',
+        'custom-green' : '#00224D'
+      }
+    },
+  },
+  plugins: [],
+}
